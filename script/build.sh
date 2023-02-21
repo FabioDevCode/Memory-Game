066b720f811bbb95beaa181f593a72a6372ccba9
FabioDevCode <0677384849@orange.fr>
2023-02-21 20:39:04 +0100

    install esbuild & add minify script

--- script/build.sh
+++ script/build.sh
@@ -6,5 +6,9 @@ mkdir assets
 cd ../
 cp index.html dist/index.html
 cp assets/*.* ./dist/assets/
-sass scss/style.scss:dist/css/style.css
+sass scss/style.scss:dist/css/style.css --style compressed
 npx tsc --project tsconfig.production.json
+cd dist
+esbuild app.js --minify --allow-overwrite --outfile=app.js
+cd css
+rm style.css.map
