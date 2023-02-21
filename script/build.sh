e353d7da21e706988e84940e8b2f783fb0dfa325
FabioDevCode <0677384849@orange.fr>
2023-02-21 20:54:10 +0100

    add data.json into build script

--- script/build.sh
+++ script/build.sh
@@ -3,9 +3,11 @@
 mkdir dist
 cd dist
 mkdir assets
+mkdir data
 cd ../
 cp index.html dist/index.html
 cp assets/*.* ./dist/assets/
+cp data/data.json ./dist/data/
 sass scss/style.scss:dist/css/style.css --style compressed
 npx tsc --project tsconfig.production.json
 cd dist
