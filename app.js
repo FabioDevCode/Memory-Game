7409ccbea180a64297a63c3cb88078f873a0ff8c
FabioDevCode <0677384849@orange.fr>
2023-02-25 00:18:16 +0100

    In progress

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 "use strict";
-console.log("MEMORY GAME");
 const btns = document.querySelectorAll('.btn-grid');
-// Function avant partie
+const select = document.querySelector('#select-theme');
+const play = document.querySelector('.btn-play');
+// Select options
+fetch('../data/data.json')
+    .then(res => res.json())
+    .then(data => {
+    data.theme.forEach((el) => {
+        const option = document.createElement('option');
+        option.value = el;
+        option.innerHTML = el;
+        select === null || select === void 0 ? void 0 : select.appendChild(option);
+    });
+});
+// Grid btns
 btns.forEach((el) => {
     el.addEventListener('click', () => {
         btns.forEach((el) => {
             el.classList.remove('active');
         });
         el.classList.toggle('active');
+    });
+});
+// Btn play
+play === null || play === void 0 ? void 0 : play.addEventListener('click', function () {
+    btns.forEach(el => {
+        var _a;
+        if ((_a = el.getAttribute('class')) === null || _a === void 0 ? void 0 : _a.includes('active')) {
+            console.log(el.getAttribute('value'));
+        }
     });
 });
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,9 +1,20 @@
-console.log("MEMORY GAME");
 const btns: NodeListOf<Element> = document.querySelectorAll('.btn-grid');
+const select: Element|null = document.querySelector('#select-theme');
+const play: Element|null = document.querySelector('.btn-play');
 
+// Select options
+fetch('../data/data.json')
+.then(res => res.json())
+.then(data => {
+    data.theme.forEach((el: any): void => {
+        const option: HTMLOptionElement = document.createElement('option');
+        option.value = el;
+        option.innerHTML = el;
+        select?.appendChild(option);
+    });
+});
 
-
-// Function avant partie
+// Grid btns
 btns.forEach((el: Element): void => {
     el.addEventListener('click', (): void => {
         btns.forEach((el: Element): void => {
@@ -12,3 +23,45 @@ btns.forEach((el: Element): void => {
         el.classList.toggle('active');
     });
 });
+
+// Btn play
+play?.addEventListener('click', function() {
+
+    btns.forEach(el => {
+        if(el.getAttribute('class')?.includes('active')) {
+           console.log(el.getAttribute('value'));
+        }
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
