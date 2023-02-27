584d8b81630f4300ed07db05d895583dcaf312aa
FabioDevCode <0677384849@orange.fr>
2023-02-27 21:27:40 +0100

    test change build script

--- _script/build.sh
+++ _script/build.sh
@@ -10,5 +10,3 @@ cp -r ./assets ./dist
 cp data/data.json ./dist/data/
 sass scss/style.scss:dist/css/style.css --style compressed
 npx tsc --project tsconfig.config.json
-cd dist
-esbuild app.js --minify --allow-overwrite --outfile=app.js
